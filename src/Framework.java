73a6dc0f710e46e3fb3cef330531b40e8eead67f
marmikpanchal <marmik.panchal@gmail.com>
2018-10-03 21:28:29 +1000

    Added R code for inductrdr

--- src/Framework.java
+++ src/Framework.java
@@ -23,19 +23,35 @@ public class Framework {
 	
 	public static void main(String[] args) throws FileNotFoundException, IOException, InvalidFormatException {
 		System.out.println("\n\nWelcome to Bitcoin Price Prediction Framework!\n\n");
-		//runner();
+		runner();
 		
-		String my[] = { "--vanilla" };
-		Rengine re = new Rengine(my, false, null);
-		re.eval(String.format("g <- '%s'", "Hello R..."));
-		REXP result = re.eval("g");
-		System.out.println("Greeting R test...: " + result);
+//		String my[] = { "--vanilla" };
+//		Rengine re = new Rengine(my, false, null);
+//		re.eval("library(inductrdr)");
+//		re.eval("library(Rcpp)");
+//		String traincsv = "C:/Users/student/Desktop/Thesis/traincsv.csv";
+//		re.assign("train", traincsv);
+//		re.eval("trainData <- read.csv(train)");
+//		re.eval("vec <- c(2,2,2,1,1,2,0)");
+//		re.eval("trainRules <- createRules(trainData, vec)");
+//		REXP result = re.eval("capture.output(trainRules)");
+//		String[] output = result.asStringArray();
+//		for (String s : output) {
+//			System.out.println(s);
+//		}
 		
-		String javaVector = "c(1,2,3,4,5)";
-		re.eval("rVector="+javaVector);
-		re.eval("meanVal=mean(rVector)");
-		double mean = re.eval("meanVal").asDouble();
-		System.out.println("Mean of the vector is=" + mean);
+		
+		
+		
+		//re.eval(String.format("g <- '%s'", "Hello R..."));
+		//REXP result = re.eval("g");
+		//System.out.println("Greeting R test...: " + result);
+		
+		//String javaVector = "c(1,2,3,4,5)";
+		//re.eval("rVector="+javaVector);
+		//re.eval("meanVal=mean(rVector)");
+		//double mean = re.eval("meanVal").asDouble();
+		//System.out.println("Mean of the vector is=" + mean);
 		
 		return;
 	}
@@ -46,7 +62,7 @@ public class Framework {
 		System.out.println("2. Configure testing dataset");
 		System.out.println("3. Generate Rules [NOTE: choose training dataset first]");
 		System.out.println("4. Modify Rules [NOTE: choose training dataset first]");
-		System.out.println("5. Test Prediction Accuracy [NOTE: choose training and testing datasets first]");
+		System.out.println("5. Predict Test Dataset and Display Results [NOTE: choose training and testing datasets first]");
 		System.out.println("6. EXIT\n\n");
 		System.out.print("CHOOSE YOUR OPTION: ");
 
@@ -63,6 +79,9 @@ public class Framework {
 			} 
 			else if (choice == 2) {
 				configTestingDataset(reader);
+			}
+			else if (choice == 3) {
+				generateRules();
 			}
 			else if (choice == 6) {
 				System.out.println("\nExiting the Framework!");
@@ -122,6 +141,21 @@ public class Framework {
 	}
 	
 	
+	public static void generateRules(Rengine re) {
+		
+		String traincsv = "C:/Users/student/Desktop/Thesis/traincsv.csv";
+		re.assign("train", traincsv);
+		re.eval("trainData <- read.csv(train)");
+		re.eval("vec <- c(2,2,2,1,1,2,0)");
+		re.eval("trainRules <- createRules(trainData, vec)");
+		re.eval("rules <- outputRules(trainRules)");
+		REXP result = re.eval("capture.output(rules)");
+		String[] output = result.asStringArray();
+		for (String s : output) {
+			System.out.println(s);
+		}
+		
+	}
 	
 	
 	public static void loadFile(String filename, ArrayList<Data> dataset) throws FileNotFoundException, IOException, InvalidFormatException {
